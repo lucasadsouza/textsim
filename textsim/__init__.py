9dfd6ae4bdfe93c71077bcbd8f85483448a92111
lucasadsouza <lucasadsouza.dev@gmail.com>
2022-11-29 22:23:55 -0300

    Added directories, main __init__.py and setup.py files.

---
+++ textsim/__init__.py
@@ -0,0 +1,12 @@
+"""
+TextSim - Text similarity tools
+
+A module that offers text similarity tools.
+
+"""
+
+__project__ = 'textsim'
+__author__ = 'lucasadsouza'
+__authoremail__ = 'lucasadsouza.dev@gmail.com'
+__url__ = 'https://github.com/lucasadsouza/textsim'
+__version__ = '1.0.0'
